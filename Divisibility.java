7811ed347450a01e6594fecd1f5094b71f640f84
Meghana.U.S <78945900+usmeg@users.noreply.github.com>
2022-09-05 19:58:54 +0530

    Add files via upload

---
+++ Divisibility.java
@@ -0,0 +1,55 @@
+/*You are provided an array  of size  that contains non-negative integers. Your task is to determine whether the number that is formed by selecting the last digit of all the N numbers is divisible by .
+
+Note: View the sample explanation section for more clarification.
+
+Input format
+
+First line: A single integer  denoting the size of array 
+Second line:  space-separated integers.
+Output format
+
+If the number is divisible by , then print . Otherwise, print .
+
+Constraints
+
+Sample Input
+5
+85 25 65 21 84
+Sample Output
+No
+Time Limit: 1
+Memory Limit: 256
+Source Limit:
+Explanation
+Last digit of 85 is 5.
+Last digit of 25 is 5.
+Last digit of 65 is 5.
+Last digit of 21 is 1.
+Last digit of 84 is 4.
+Therefore the number formed is 55514  which is not divisible by 10
+
+My logic- The divisibility rule for 10 is the last digit must be 0 or the number must be even and divisible by 5
+Taking just the last input's last digit and checking divisibility rule
+//Passed all the test cases 
+*/
+import java.util.*;
+class Divisibility{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s="";
+        int num;
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextInt();
+            int r = arr[i] % 10;
+            s=s+r;
+        }
+        num = Integer.valueOf(s.charAt(s.length()-1));
+        if(num==0) System.out.println("Yes");
+        else{
+            System.out.println("No");
+        }
+        sc.close();    
+    }
+}
---
+++ MonkQuotient.java
@@ -0,0 +1,74 @@
+/*Problem
+The Monk wants to teach all its disciples a lesson about patience, since they are always in a hurry to do something crazy. To teach them this, he gives them a list of N numbers, which may or may not be distinct. The students are supposed to solve a simple Mathematical equation based on the array of these N numbers.
+
+g(x) - GCD (a[ 0 ], a[ 1 ], a[ 2 ]... a[n-1] )
+f(x) - (a[ 0 ] * a[ 1 ] * a[ 2 ]... * a[n-1] )
+The value of the MonkQuotient is: 109 + 7.
+
+The equation to be solved is: ( f(x)g(x) ) % MonkQuotient
+
+Input constraints:
+The first line of input will contain an integer — N. The next line will contain N integers denoting the elements of the list.
+
+Output constraints:
+Print the required answer of the equation.
+
+Constraints:
+1 ≤ N ≤ 50
+1 ≤ Ai ≤ 103
+
+Sample Input
+2
+2 6
+Sample Output
+144
+Time Limit: 2
+Memory Limit: 256
+Source Limit:
+Explanation
+Here we can see that the product of the elements of array is 12 and the GCD of the array comes out to be 2 . Thus the answer would be 12^2 which is 144.*/
+//passed all test cases 
+import java.util.*;
+class MonkQuotient{
+    public static void main(String args[]){
+        int N; double gcd=1d; double m=1d;
+        long monk = 1000000007;
+        Scanner sc = new Scanner(System.in);
+        MonkQuotient obj = new MonkQuotient();
+        N=sc.nextInt();
+        double arr[]=new double[N];
+
+        for(int i=0;i<N;i++){
+            arr[i]=sc.nextDouble();
+            m=(m*arr[i]) % monk;
+        }
+        sc.close();
+
+        gcd=arr[0];
+		for(int i=1;i<N;i++)
+		{
+			gcd=obj.GCD(gcd,arr[i]);
+			if(gcd==1)
+				break;
+		}
+
+		double a=0;
+		if(gcd>1)
+		{
+		    a=m;
+			for(int i=1;i<gcd;i++)
+			{
+				m=(m*a)%monk;
+			}
+		}
+		System.out.println((int)m);
+		
+		
+	}
+	double GCD(double a, double b) {
+		if(b==0)
+			return a;
+		return GCD(b,a%b);
+	}
+
+}
